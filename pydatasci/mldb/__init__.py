2c1e65befc900a3cac90a58f276daadb242194cc
Layne Sadler <layne@macbook.local>
2020-09-12 10:46:44 -0400

    checking permissions for the user

--- pydatasci/mldb/__init__.py
+++ pydatasci/mldb/__init__.py
@@ -1,65 +1,104 @@
 name = "mldb"
 
-import os, sqlite3
+import os, getpass, sqlite3
 from os import path
 
 import appdirs
 
-# add a whoami for helping troubleshoot and checking permissions?
-# make this printing a func()
-# print("\n=> Check that the operating system user that you are logged in as\n  has permission to create files at path:\n" + app_dir)
-# is $USER universal?
+
+def check_permissions(path:str):
+	# learned that pip reads from appdirs on macos. so if they ran pip to install it they can r/w?
+	readable = os.access(path, os.R_OK)
+	writeable = os.access(path, os.R_OK)
+
+	if not readable:
+		print("\n=> Error - your operating system userID does not have permission to read from path:\n" + path + "\n")
+		print("\n=> Fix - you can attempt to fix this by running `mldb.grant_appdirs_permissions()`.\n")
+		return False
+	elif not writeable:
+		print("\n=> Error - your operating system userID does not have permission to read from path:\n" + path + "\n")
+		print("\n=> Fix - you can attempt to fix this by running `mldb.grant_appdirs_permissions()`\n")
+		return False
+	elif readable and writeable:
+		return True
+
+
+def grant_appdirs_permissions():
+	app_dir = appdirs.user_data_dir()
+	command = "chmod +wr "
+	full_command = command + '"' + app_dir + '"'
+
+	try:
+		sys_response = os.system(full_command)
+	except:
+		print("\nError - error failed to execute this system command: " + full_command +"\n")
+	
+	permissions = check_permissions(path=app_dir)
+	if permissions == True:
+		print("\nSuccess - operating system userID can read and write from path: " + app_dir + "\n")
+	else:
+		print("\nError - Failed to grant operating system userID permission to read and write from path: " + app_dir + "\n")
+
 
 def create_db():
+	# ToDo - Could let the user specify their own db name, for import tutorials.
+	# ToDo - Need to know how to reference the default path globally.
+	db_file_name = "pydatasci_mldb.sqlite3"
+	app_dir = appdirs.user_data_dir()
+	full_db_path = app_dir + db_file_name
+
+	permissions = check_permissions(path=app_dir)
+
+	if permissions == True:
+		# check if file exists already
+		path_exists = path.exists(full_db_path)
+		if (path_exists == True):
+			print("\n=> Warning - skipping creation as a database already exists at path:\n" + full_db_path + "\n")
+		else:
+			# attempt to create it
+			try:
+				conn = sqlite3.connect(full_db_path)
+				del conn
+			except:
+				print("\n=> Error - failed to create database file at path:\n" + full_db_path)
+				print("===================================\n")
+				raise
+
+			# verify its creation
+			path_exists = path.exists(full_db_path)
+			if (path_exists == True):
+				print("\n=> Success - created database for machine learning metrics at path:\n" + full_db_path + "\n")
+				
+				print("=> Success - Returning string of the path so that it can be set as a variable:")
+				return full_db_path
+			else:
+				print("\n=> Error - failed to create database at path:\n" + full_db_path + "\n")
+	
+	# ToDo - Need to create the tables
+
+
+def delete_db(confirm:bool):
 	# Could let the user specify their own db name, for import tutorials.
 	# Need to know how to reference the default path globally.
 	db_file_name = "pydatasci_mldb.sqlite3"
-	full_db_path = appdirs.user_data_dir(db_file_name)
 	app_dir = appdirs.user_data_dir()
-
-	# check if file exists already
-	path_exists = path.exists(full_db_path)
-	if (path_exists == True):
-		print("\n=> Warning - skipping creation as a database already exists at path:\n" + full_db_path + "\n")
-	else:
-		# attempt to create it
-		try:
-			conn = sqlite3.connect(full_db_path)
-			del conn
-		except:
-			print("\n=> Warning - failed to create database file at path:\n" + full_db_path)
-			print("===================================\n")
-			raise
-
-		# verify its creation
-		path_exists = path.exists(full_db_path)
-		if (path_exists == True):
-			print("\n=> Successfully created database for machine learning metrics at path:\n" + full_db_path + "\n")
-			
-			print("=> Returning string of the path so that it can be set as a variable:")
-			return full_db_path
-		else:
-			print("\n=> Warning - failed to create database at path:\n" + full_db_path + "\n")
-
-	# Need to create the tables
-
-def delete_db(confirm:bool):
-	# Need to know how to reference the default path globally.
-	db_file_name = "pydatasci_mldb.sqlite3"
-	full_db_path = appdirs.user_data_dir(db_file_name)
+	full_db_path = app_dir + db_file_name
 
 	if (confirm == True):		
-		path_exists = path.exists(full_db_path)
-		if (path_exists == True):
-			try:
-				os.remove(full_db_path)
-			except:
-				print("\n=> Warning - failed to delete database at path:\n" + full_db_path)
-				print("===================================")
-				raise
-			print("\n=> Successfully deleted database at path:\n" + full_db_path + "\n")	
-		else:
-			print("\n=> Warning - there is no file to delete at path:\n" + full_db_path + "\n")
+		permissions = check_permissions(path=app_dir)
+
+		if permissions == True:
+			path_exists = path.exists(full_db_path)
+			if (path_exists == True):
+				try:
+					os.remove(full_db_path)
+				except:
+					print("\n=> Error - failed to delete database at path:\n" + full_db_path)
+					print("===================================")
+					raise
+				print("\n=> Success - deleted database at path:\n" + full_db_path + "\n")	
+			else:
+				print("\n=> Warning - there is no file to delete at path:\n" + full_db_path + "\n")
 
 	else:
 		print("\n=> Warning - skipping deletion because `confirm` arg not set to boolean `True`.\n")
